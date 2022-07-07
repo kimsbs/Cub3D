03cf42d8ddf7f33e820f87cc2bcde4653b8801d9
42hena <somnium1108@gmail.com>
2022-07-07 16:41:51 +0900

    change : value name
    - dirX -> dir_x
    - dirY -> dir_y
    - planeX -> plane_x
    - planeY -> plane_y

--- srcs/ft_key_arrow.c
+++ srcs/ft_key_arrow.c
@@ -5,14 +5,14 @@ void	ft_key_arrow_left(t_info *data)
 	double	old_dir_x;
 	double	old_plane_x;
 
-	old_dir_x = data->dirX;
-	old_plane_x = data->planeX;
-	data->dirX = data->dirX * cos(-ROT_SPEED) - data->dirY * sin(-ROT_SPEED);
-	data->dirY = old_dir_x * sin(-ROT_SPEED) + data->dirY * cos(-ROT_SPEED);
-	data->planeX = data->planeX * cos(-ROT_SPEED)
-		- data->planeY * sin(-ROT_SPEED);
-	data->planeY = old_plane_x * sin(-ROT_SPEED)
-		+ data->planeY * cos(-ROT_SPEED);
+	old_dir_x = data->dir_x;
+	old_plane_x = data->plane_x;
+	data->dir_x = data->dir_x * cos(-ROT_SPEED) - data->dir_y * sin(-ROT_SPEED);
+	data->dir_y = old_dir_x * sin(-ROT_SPEED) + data->dir_y * cos(-ROT_SPEED);
+	data->plane_x = data->plane_x * cos(-ROT_SPEED)
+		- data->plane_y * sin(-ROT_SPEED);
+	data->plane_y = old_plane_x * sin(-ROT_SPEED)
+		+ data->plane_y * cos(-ROT_SPEED);
 }
 
 void	ft_key_arrow_right(t_info *data)
@@ -20,12 +20,12 @@ void	ft_key_arrow_right(t_info *data)
 	double	old_dir_x;
 	double	old_plane_x;
 
-	old_dir_x = data->dirX;
-	old_plane_x = data->planeX;
-	data->dirX = data->dirX * cos(ROT_SPEED) - data->dirY * sin(ROT_SPEED);
-	data->dirY = old_dir_x * sin(ROT_SPEED) + data->dirY * cos(ROT_SPEED);
-	data->planeX = data->planeX * cos(ROT_SPEED)
-		- data->planeY * sin(ROT_SPEED);
-	data->planeY = old_plane_x * sin(ROT_SPEED)
-		+ data->planeY * cos(ROT_SPEED);
+	old_dir_x = data->dir_x;
+	old_plane_x = data->plane_x;
+	data->dir_x = data->dir_x * cos(ROT_SPEED) - data->dir_y * sin(ROT_SPEED);
+	data->dir_y = old_dir_x * sin(ROT_SPEED) + data->dir_y * cos(ROT_SPEED);
+	data->plane_x = data->plane_x * cos(ROT_SPEED)
+		- data->plane_y * sin(ROT_SPEED);
+	data->plane_y = old_plane_x * sin(ROT_SPEED)
+		+ data->plane_y * cos(ROT_SPEED);
 }
