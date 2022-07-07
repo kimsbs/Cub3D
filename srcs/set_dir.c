a0404eb48a6cd9bf68dfa7dde4af9dcf158f6eab
42hena <somnium1108@gmail.com>
2022-07-07 16:38:03 +0900

    Change : value name
    - dirX -> dir_x
    - dirY -> dir_y
    - planeX -> plane_x
    - planeY -> plane_y

--- srcs/set_dir.c
+++ srcs/set_dir.c
@@ -2,34 +2,34 @@
 
 void	set_north(t_info *data)
 {
-	data->dirX = 0.0;
-	data->dirY = -1.0;
-	data->planeX = 0.66;
-	data->planeY = 0.0;
+	data->dir_x = 0.0;
+	data->dir_y = -1.0;
+	data->plane_x = 0.66;
+	data->plane_y = 0.0;
 }
 
 void	set_south(t_info *data)
 {
-	data->dirX = 0.0;
-	data->dirY = 1.0;
-	data->planeX = -0.66;
-	data->planeY = 0.0;
+	data->dir_x = 0.0;
+	data->dir_y = 1.0;
+	data->plane_x = -0.66;
+	data->plane_y = 0.0;
 }
 
 void	set_west(t_info *data)
 {
-	data->dirX = -1.0;
-	data->dirY = 0.0;
-	data->planeX = 0.0;
-	data->planeY = -0.66;
+	data->dir_x = -1.0;
+	data->dir_y = 0.0;
+	data->plane_x = 0.0;
+	data->plane_y = -0.66;
 }
 
 void	set_east(t_info *data)
 {
-	data->dirX = 1.0;
-	data->dirY = 0.0;
-	data->planeX = 0.0;
-	data->planeY = 0.66;
+	data->dir_x = 1.0;
+	data->dir_y = 0.0;
+	data->plane_x = 0.0;
+	data->plane_y = 0.66;
 }
 
 void	set_dir_with_vec(t_info *data)
